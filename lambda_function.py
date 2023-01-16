7d1c95ab336eea5f5649a1c43cd3e269fd462b3b
zanaborio <zanaborio21@gmail.com>
2023-01-16 17:07:14 -0500

    primer commit

---
+++ lambda_function.py
@@ -0,0 +1,23 @@
+import json
+import os
+import mercadopago
+
+    
+
+def lambda_handler(event, context):
+    sdk = mercadopago.SDK(os.environ["ACCESS_TOKEN"])
+    bodyGet=json.loads(event["body"])
+    
+    
+    payment_response = sdk.payment().create(bodyGet)
+    payment = payment_response["response"]
+    json_payment=json.dumps(payment_response['response'])
+    
+
+    print(payment)
+    print(json_payment)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(payment),
+    }
